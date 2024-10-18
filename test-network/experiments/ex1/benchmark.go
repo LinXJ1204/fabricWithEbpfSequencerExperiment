c7b79090e452b8c0030d6cf50ea86882cc70fba9
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2024-10-18 18:25:42 +0800

    feat: workload

--- test-network/experiments/ex1/benchmark.go
+++ test-network/experiments/ex1/benchmark.go
@@ -4,29 +4,36 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/hyperledger/fabric-gateway/pkg/client"
 )
 
 // Function to measure TPS for asynchronous transactions (TransferAsset)
-func measureTPSTransferAssetAsync(contract *client.Contract, numTransactions int) {
+func measureTPSTransferAssetAsync(contract *client.Contract, numTransactions int, workload uint) {
 	startTime := time.Now()
 
 	errCount := 0
+	var mu sync.Mutex // Mutex for thread-safe error count updates
 
 	for i := 0; i < numTransactions; i++ {
 		wg.Add(1)
 		assetId := "asset" + strconv.Itoa(rand.Intn(1000000)) // Generate random asset IDs
+		time.Sleep(time.Duration(1/workload*1000) * time.Millisecond)
 		go func(i int) {
 			defer wg.Done()
 			err := createAsset(contract, assetId)
 			if err != nil {
+				mu.Lock()
 				errCount++
+				mu.Unlock()
 			} else {
 				err = transferAssetAsync(contract, assetId) // Submit an asynchronous transaction
 				if err != nil {
+					mu.Lock()
 					errCount++
+					mu.Unlock()
 				}
 			}
 		}(i)
