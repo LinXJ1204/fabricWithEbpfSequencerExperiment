da04df36e4037847d7fcea4454c387a2d639378f
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2024-11-19 21:41:27 +0800

    feat: add block listenning

---
+++ test-network/experiments/TPSmeasure/main.go
@@ -0,0 +1,190 @@
+/*
+Copyright 2021 IBM All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"context"
+	"crypto/x509"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	mspID        = "Org1MSP"
+	cryptoPath   = "../../organizations/peerOrganizations/org1.example.com"
+	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts"
+	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore"
+	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
+	peerEndpoint = "dns:///localhost:13051"
+	gatewayPeer  = "peer1.org1.example.com"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) // Adjust duration here
+	defer cancel()
+
+	// The gRPC client connection should be shared by all Gateway connections to this endpoint
+	clientConnection := newGrpcConnection()
+	defer clientConnection.Close()
+
+	id := newIdentity()
+	sign := newSign()
+
+	// Create a Gateway connection for a specific client identity
+	gw, err := client.Connect(
+		id,
+		client.WithSign(sign),
+		client.WithClientConnection(clientConnection),
+		// Default timeouts for different gRPC calls
+		client.WithEvaluateTimeout(5*time.Second),
+		client.WithEndorseTimeout(15*time.Second),
+		client.WithSubmitTimeout(5*time.Second),
+		client.WithCommitStatusTimeout(1*time.Minute),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer gw.Close()
+
+	channelName := "mychannel"
+	if cname := os.Getenv("CHANNEL_NAME"); cname != "" {
+		channelName = cname
+	}
+
+	network := gw.GetNetwork(channelName)
+
+	blockChannel, err := network.BlockEvents(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	measureTPS(blockChannel)
+}
+
+// newGrpcConnection creates a gRPC connection to the Gateway server.
+func newGrpcConnection() *grpc.ClientConn {
+	certificatePEM, err := os.ReadFile(tlsCertPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read TLS certifcate file: %w", err))
+	}
+
+	certificate, err := identity.CertificateFromPEM(certificatePEM)
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(certificate)
+	transportCredentials := credentials.NewClientTLSFromCert(certPool, gatewayPeer)
+
+	connection, err := grpc.Dial(peerEndpoint, grpc.WithTransportCredentials(transportCredentials))
+	if err != nil {
+		panic(fmt.Errorf("failed to create gRPC connection: %w", err))
+	}
+
+	return connection
+}
+
+// newIdentity creates a client identity for this Gateway connection using an X.509 certificate.
+func newIdentity() *identity.X509Identity {
+	certificatePEM, err := readFirstFile(certPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read certificate file: %w", err))
+	}
+
+	certificate, err := identity.CertificateFromPEM(certificatePEM)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err := identity.NewX509Identity(mspID, certificate)
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+// newSign creates a function that generates a digital signature from a message digest using a private key.
+func newSign() identity.Sign {
+	privateKeyPEM, err := readFirstFile(keyPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key file: %w", err))
+	}
+
+	privateKey, err := identity.PrivateKeyFromPEM(privateKeyPEM)
+	if err != nil {
+		panic(err)
+	}
+
+	sign, err := identity.NewPrivateKeySign(privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return sign
+}
+
+func readFirstFile(dirPath string) ([]byte, error) {
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	fileNames, err := dir.Readdirnames(1)
+	if err != nil {
+		return nil, err
+	}
+
+	return os.ReadFile(path.Join(dirPath, fileNames[0]))
+}
+
+func measureTPS(blocks <-chan *common.Block) {
+	startTime := time.Now()
+	var totalTransactions int
+
+	for blockEvent := range blocks {
+		bc := blockEvent.GetData()
+		for _, data := range bc.Data {
+			env := &common.Envelope{}
+			if err := proto.Unmarshal(data, env); err != nil {
+				fmt.Printf("Failed to unmarshal envelope: %v\n", err)
+				continue
+			}
+
+			payload := &common.Payload{}
+			if err := proto.Unmarshal(env.Payload, payload); err != nil {
+				fmt.Printf("Failed to unmarshal payload: %v\n", err)
+				continue
+			}
+
+			chHeader := &common.ChannelHeader{}
+			if err := proto.Unmarshal(payload.Header.ChannelHeader, chHeader); err != nil {
+				fmt.Printf("Failed to unmarshal channel header: %v\n", err)
+				continue
+			}
+
+			if chHeader.Type == int32(common.HeaderType_ENDORSER_TRANSACTION) {
+				totalTransactions++
+				fmt.Println(totalTransactions)
+			}
+		}
+	}
+
+	duration := time.Since(startTime).Seconds()
+	tps := float64(totalTransactions) / duration
+	fmt.Printf("Total Transactions: %d, Duration: %f, TPS: %f\n", totalTransactions, duration, tps)
+}
