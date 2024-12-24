59181ec20c65158b0dda824dfe035efecfcbe5db
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2024-12-24 16:05:11 +0800

    feat: auto deployment v9

--- test-network/boostrapScripts/autoDeployment/autoDeployment.sh
+++ test-network/boostrapScripts/autoDeployment/autoDeployment.sh
@@ -189,7 +189,7 @@ echo "===================================================="
 echo " Deployment steps completed successfully!"
 echo "===================================================="
 
-run_cmd_on_device 0 "cd mainPlan/fabricWithEbpfSequencerExperiment/test-network/experiments && export GO111MODULE=on && go mod tidy && go run ."
+run_cmd_on_device 0 "cd mainPlan/fabricWithEbpfSequencerExperiment/test-network/experiments/initLedger && export GO111MODULE=on && go mod tidy && go run ."
 sleep 5
 
 echo ""
